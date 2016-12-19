a325fbf8e99b036a29dc0e4ff52012ce63276db1
Anders Hansen <pareion12@gmail.com>
2016-12-19 09:22:22 +0100

    Last project finished

---
+++ MVC_PracticeExam_02_Solution/AuctionWebApplication/Controllers/AuktionController.cs
@@ -0,0 +1,60 @@
+﻿using AuctionWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuctionWebApplication.Controllers
+{
+    public class AuktionController : Controller
+    {
+        AuktionServiceReference.AuctionsServiceClient service = new AuktionServiceReference.AuctionsServiceClient();
+        // GET: Auktion
+        [HttpGet]
+        public ActionResult ShowAllAuktions()
+        {
+            return View(service.GetAllAuctionItems());
+        }
+        [HttpGet]
+        public ActionResult Bid(int id)
+        {
+            Bid b = new Bid();
+            b.Auktion = service.GetAuctionItem(id);
+            Session["Id"] = id;
+            Session["CurrentBid"] = b.Auktion;
+            return View(b);
+        }
+        [HttpPost]
+        public ActionResult Bid(Bid bid)
+        {
+            if (Session["CurrentBid"] == null)
+            {
+                return RedirectToAction("ShowAllAuktions");
+            }
+            bid.Auktion = (AuktionServiceReference.AuctionItem)Session["CurrentBid"];
+            bid.Auktion.BidTimestamp = DateTime.Now;
+            service.ProvideBid(bid.Auktion.ItemNumber, bid.bidPrice, bid.bidCustomName, bid.bidCustomPhone.ToString());
+            int id = int.Parse(Session["Id"].ToString());
+            return RedirectToAction("Bid", id);
+        }
+        [HttpPost]
+        public ActionResult SeAlleBud(string username)
+        {
+            if (username == null)
+            {
+                return RedirectToAction("ShowAllAuktions");
+            }
+            List<AuktionServiceReference.AuctionItem> result = new List<AuktionServiceReference.AuctionItem>();
+            foreach (var item in service.GetAllAuctionItems())
+            {
+                if (item.BidCustomName == username)
+                {
+                    result.Add(item);
+                }
+            }
+            return View(result);
+        }
+
+    }
+}
---
+++ MVC_PracticeExam_02_Solution/AuctionWebApplication/Models/Bid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuctionWebApplication.Models
+{
+    public class Bid
+    {
+        [Required]
+        public int itemNumber { get; set; }
+        [Required]
+        public int bidPrice { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string bidCustomName { get; set; }
+        [Required]
+        [Range(8,8)]
+        public int bidCustomPhone { get; set; }
+        public AuktionServiceReference.AuctionItem Auktion { get; set; }
+    }
+}
